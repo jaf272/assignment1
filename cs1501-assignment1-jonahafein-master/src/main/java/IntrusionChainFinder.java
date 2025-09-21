fda8b59ce9518a5ae53757d918d013ccee3fe80f
jaf272 <jaf272@pitt.edu>
2025-09-21 00:03:01 +0000

    starting to find allowed routes method

--- cs1501-assignment1-jonahafein-master/src/main/java/IntrusionChainFinder.java
+++ cs1501-assignment1-jonahafein-master/src/main/java/IntrusionChainFinder.java
@@ -42,13 +42,14 @@ public final class IntrusionChainFinder {
   }
 
   private static boolean sufficient_priviledge(Priv have, Priv need){
-    int have_numeric = priviledge_order(have);
-    int need_numeric = priviledge_order(need);
-    
     // no priviledge needed case
     if(need == null){
       return true;
     }
+    // converting to numeric
+    int have_numeric = priviledge_order(have);
+    int need_numeric = priviledge_order(need);
+
 
     if(have_numeric >= need_numeric){
       return true;
@@ -56,6 +57,57 @@ public final class IntrusionChainFinder {
     else{
       return false;
     }
+  }
+
+  // method to see if attacker has credential in inventory
+  private static boolean has_cred_with_tag(Set<String> inventory, String tag){
+    // case where no tag is required
+    if(tag == null){
+      return true;
+    }
+    if(tag.equals("")){
+      return true;
+    }
+    // empty inventory case
+    if(inventory == null){
+      return false;
+    }
+
+    // seeing if any credential starts with tag
+    for(String cred:inventory){
+      if(cred.startsWith(tag)){
+        return true;
+      }
+    }
+
+    return false;
+
+  }
+
+  // find a route from and to that allows the svc service
+  private static Route find_route_allowing(SystemInfo from, SystemInfo to, String svc){
+
+    // if info missing we can't find a route
+    if(from == null || to == null || svc == null){
+      System.out.println("Missing a value cannot find a route");
+      return null;
+    }
+
+    // if no routes defined from from, return null
+    if(from.routes == null || from.routes.size() == 0){
+      System.out.println("No outgoing routes from" + from.name);
+      return null;
+    }
+
+
+    // now looking at each outgoing route from the from 
+    for(int i = 0; i<from.routes.size(); i++){
+      Route r = from.routes.get(i);
+
+
+    }
+
+
   }
 
 
